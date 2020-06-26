7d10e112fda9edfa5b38713260365bae73546d40
Chris Doyle <chrisdoyle20@gmail.com>
2020-06-26 20:00:22 +0100

    New feature
    formatNum method added to prevent floats from printing with '.0'
    
    Added new input for an increment amount, this allows the use of floating point numbers and rounds to 2 decimal places. User input validation still required.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,27 +1,37 @@
 print("Welcome to the FizzBuzzer. Traditionally FizzBuzz is played by counting up from 1 and replacing every multiple of 3 with 'Fizz', " +
 "every multiple of 5 with 'Buzz' and every multiple of both with 'FizzBuzz'. The FizzBuzzer allows you to create a custom version of this " +
-"game in which you can replace 'Fizz', 'Buzz', 3, and 5 with words and numbers of your choice across a defined range of integers. Let's begin...")
+"game in which you can replace 'Fizz', 'Buzz', 3, and 5 with words and numbers of your choice and increment by a specified amount across a " + 
+"defined range. Let's begin...")
+
+#method to prevent outputting floats such as 12.0
+def formatNum(num):
+    if num % 1 == 0:
+        return int(num)
+    else:
+        return num
 
 #initialise empty list and ask for all necessary string inputs
 #TODO: add data validation to rangeMin, rangeMax, firstNum and secondNum
 resultList = []
-rangeMin = int(input("Choose first number in range: "))
-rangeMax = int(input("Choose last number in range: "))
+rangeMin = float(input("Choose first number in range: "))
+rangeMax = float(input("Choose last number in range: "))
 firstWord = input("Choose first word: ")
 secondWord = input("Choose second word: ")
-firstNum = int(input("Choose first divisor: "))
-secondNum = int(input("Choose second divisor: "))
+incAmount = float(input("Choose increment amount: "))
+firstNum = float(input("Choose first divisor: "))
+secondNum = float(input("Choose second divisor: "))
 
 #loop to check each number against divisors and append appropriate value to list
-for i in range(rangeMin, rangeMax + 1):
-    if (i % firstNum == 0) and (i % secondNum == 0):
+while rangeMin <= rangeMax:
+    if (rangeMin % firstNum == 0) and (rangeMin % secondNum == 0):
         resultList.append(firstWord + secondWord)
-    elif i % firstNum == 0:
+    elif rangeMin % firstNum == 0:
         resultList.append(firstWord)
-    elif i % secondNum == 0:
+    elif rangeMin % secondNum == 0:
         resultList.append(secondWord)
     else:
-        resultList.append(str(i))
+        resultList.append(str(formatNum(round(rangeMin, 2))))
+    rangeMin = rangeMin + incAmount
 
 #FizzBuzz replaced with chosen words
 print("Here is your customised " + firstWord + secondWord + ":")
